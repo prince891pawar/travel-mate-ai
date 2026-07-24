63b5aedea63e15f8030922b623323d0b077110a6
prince pawar <princepawar891@gmail.com>
2026-07-24 21:47:23 +0530

    update

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,13 +1,37 @@
-import {createContext, useContext, useState} from "react";
+import {createContext,useEffect, useState} from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => {
   const [user, setUser] = useState(null);
     const [token, setToken] = useState(null);
+    
+    const login = (userData, newToken) => {
+        setUser(userData);
+        setToken(newToken);
+        localStorage.setItem("token", newToken); 
+        localStorage.setItem("user", JSON.stringify(userData));
+    };
+
+    const logout = () => {
+        setUser(null);
+        setToken(null);
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+    };
+
+    useEffect(() => {
+  const token = localStorage.getItem("token");
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  if (token && user) {
+    setToken(token);
+    setUser(user);
+  }
+}, []);
 
     return (
-        <AuthContext.Provider value={{user, setUser, token, setToken}}>
+        <AuthContext.Provider value={{user, login, token, logout}}>
             {children}
         </AuthContext.Provider>
     );
