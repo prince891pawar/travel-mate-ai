4e65ecc8847cb69d03ff1de88dfb0e0268391774
prince pawar <princepawar891@gmail.com>
2026-09-15 17:20:14 +0530

    update-trip-model

--- backend/src/models/Trip.js
+++ backend/src/models/Trip.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 const TripSchema = new mongoose.Schema({
-     user: {
-        type: String,
-        ref: "User",
-        required: true
-     },
+      user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
 
      destination: {
         type: String,
